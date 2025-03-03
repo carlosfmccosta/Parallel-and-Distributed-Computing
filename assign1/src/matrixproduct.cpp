8f6483cbf45ab662e8ce47cfff547d6d8f003512
carlos <carlicocosta25@gmail.com>
2025-03-03 19:14:38 +0000

    block multiplication

--- assign1/src/matrixproduct.cpp
+++ assign1/src/matrixproduct.cpp
@@ -128,7 +128,68 @@ void OnMultLine(int m_ar, int m_br)
 // add code here for block x block matriz multiplication
 void OnMultBlock(int m_ar, int m_br, int bkSize)
 {
-	return;
+	SYSTEMTIME Time1, Time2;
+
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+
+	pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for (i = 0; i < m_ar; i++)
+		for (j = 0; j < m_ar; j++)
+			pha[i * m_ar + j] = (double)1.0;
+
+	for (i = 0; i < m_br; i++)
+		for (j = 0; j < m_br; j++)
+			phb[i * m_br + j] = (double)(i + 1);
+
+	// Initialize result matrix
+    for (int i = 0; i < m_ar; i++)
+        for (int j = 0; j < m_br; j++)
+            phc[i * m_br + j] = 0.0;
+
+	Time1 = clock();
+
+	
+	for (int bi = 0; bi < m_ar; bi += bkSize) {         //block rows
+		for (int bj = 0; bj < m_br; bj += bkSize) {     //block columns
+			for (int bk = 0; bk < m_ar; bk += bkSize) { //block depth
+
+				//multiply sub-matrix (block)
+				for (int i = bi; i < min(bi + bkSize, m_ar); i++) {
+					for (int j = bj; j < min(bj + bkSize, m_br); j++) {
+						double sum = 0.0;
+						for (int k = bk; k < min(bk + bkSize, m_ar); k++) {
+							sum += pha[i * m_ar + k] * phb[k * m_br + j];
+						}
+						phc[i * m_br + j] += sum;
+					}
+				}
+			}
+		}
+	}
+
+	Time2 = clock();
+	sprintf(st, "Time: %3.9f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for (i = 0; i < 1; i++)
+	{
+		for (j = 0; j < min(10, m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+	free(pha);
+	free(phb);
+	free(phc);
 }
 
 void handle_error(int retval)
