f4d5a5dccd737dfd1048a2cc8dbcd9a359c785a8
Luciano Ferreira <luciano2607ferreira@gmail.com>
2025-05-14 17:30:11 +0100

    small changes

--- assign2/TCP_chat_sys/src/ChatSystemLauncher.java
+++ assign2/TCP_chat_sys/src/ChatSystemLauncher.java
@@ -1,22 +1,23 @@
 public class ChatSystemLauncher {
-    public static void main(String[] args) {
-        // Start chat server
-        new Thread(() -> {
+    public static void main(String[] args)
+    {
+        Thread.startVirtualThread(() -> {
             ChatServer server = new ChatServer(8080);
             server.start_server();
-        }).start();
+        });
 
-        // Start AI bot after delay for general room
-        new Thread(() -> {
-            try {
+        Thread.startVirtualThread(() -> {
+            try
+            {
                 Thread.sleep(3000); // Wait for server to start
-                AIClient bot = new AIClient("localhost", 8080,
-                        "http://localhost:11434", "llama3","general");
+                AIClient bot = new AIClient("localhost", 8080, "http://localhost:11434", "llama3", "general");
                 bot.start();
                 System.out.println("AI Bot connected");
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 e.printStackTrace();
             }
-        }).start();
+        });
     }
 }
